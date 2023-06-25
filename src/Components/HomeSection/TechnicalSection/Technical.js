3f846dd1acf42d94ddc6c9a70ca72ee67fd60317
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-06-25 00:44:30 -0400

    techstack & toolstack

---
+++ src/Components/HomeSection/TechnicalSection/Technical.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import Particle from "../../Particle";
+import Techstack from "./Techstack";
+import Toolstack from "./Toolstack";
+
+function Technical() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Particle />
+      <Container>
+        <Row>
+          <h1 className="project-heading">
+            Habilidades <strong className="purple">Técnicas </strong>
+          </h1>
+
+          <Techstack />
+
+          <h1 className="project-heading">
+            <strong className="purple">Herramientas</strong> que uso
+          </h1>
+          <Toolstack />
+          {/*
+  <Github />*/}
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Technical;
