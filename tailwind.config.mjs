217c4ffb356249f3693812072ca9e4d55cf76048
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-11-23 15:27:26 -0300

    image resources & positions
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "media",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Raleway", ...defaultTheme.fontFamily.sans], // Agrega 'Raleway' al principio de la lista
+      },
+      fontWeight: {
+        500: "500",
+      },
+    },
+  },
+  plugins: [],
+};
