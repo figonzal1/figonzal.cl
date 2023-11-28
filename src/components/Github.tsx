c5f72990c922aac099387c6686d94fcc2cdda2be
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-11-28 11:10:54 -0300

    tooltip on githubCalendar
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- src/components/Github.tsx
+++ src/components/Github.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import GitHubCalendar from "react-github-calendar";
-import { Tooltip as MuiTooltip } from "@mui/material";
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
 
 function Github() {
   const labels = {
@@ -27,23 +28,28 @@ function Github() {
   };
 
   return (
-    <GitHubCalendar
-      username="figonzal1"
-      blockSize={15}
-      blockMargin={5}
-      colorScheme="dark"
-      blockRadius={20}
-      fontSize={16}
-      labels={labels}
-	  style={{
-		color: 'white'
-	  }}
-      renderBlock={(block, activity) => (
-        <MuiTooltip title={`${activity.count} commits en ${activity.date}`}>
-          {block}
-        </MuiTooltip>
-      )}
-    />
+    <>
+      <GitHubCalendar
+        username="figonzal1"
+        blockSize={15}
+        blockMargin={5}
+        colorScheme="dark"
+        blockRadius={20}
+        fontSize={16}
+        labels={labels}
+        style={{
+          color: "white",
+        }}
+        renderBlock={(block, activity) =>
+          React.cloneElement(block, {
+            "data-tooltip-id": "react-tooltip",
+            "data-tooltip-html": `${activity.count} commits el ${activity.date}`,
+          })
+        }
+      />
+
+      <ReactTooltip id="react-tooltip" />
+    </>
   );
 }
 
