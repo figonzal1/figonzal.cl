5ef0deee440c8ab7db412ba1b5b22965380639ae
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-12-08 17:30:10 -0300

    responsive projects & refactoring
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

---
+++ src/utils/projectSeed.tsx
@@ -0,0 +1,62 @@
+import { FaGithub } from "react-icons/fa6";
+import { SiGoogleplay } from "react-icons/si";
+import type { ProjectProp } from "./types";
+
+const projectSeed: ProjectProp[] = [
+  {
+    title: "LastQuakeChile - Sismos en Chile",
+    description:
+      "Es una app donde podrás conocer los últimos sismos ocurridos en Chile y recibir notificaciones rápidamente en tu celular. Cada vez que ocurra un sismo, el servicio lastQuakeChile enviará una alerta y podrás obtener todos los detalles del sismo con sólo presionar la notificación.",
+    image: "images/Projects/lqch.png",
+    buttons: [
+      {
+        title: "Github",
+        icon: FaGithub,
+        link: "https://www.github.com/figonzal1/LastQuakeChile",
+      },
+      {
+        title: "App",
+        icon: SiGoogleplay,
+        link: "https://play.google.com/store/apps/details?id=cl.figonzal.lastquakechile",
+      },
+    ],
+  },
+  {
+    title: "AAID",
+    description:
+      "Esta aplicación permite al usuario encontrar el código AAID de su dispositivo Android con la opción de copiarlo fácilmente y compartirlo a través de otras aplicaciones.",
+    image: "images/Projects/aaid.png",
+    buttons: [
+      {
+        title: "Github",
+        icon: FaGithub,
+        link: "https://github.com/figonzal1/AAID",
+      },
+      {
+        title: "App",
+        icon: SiGoogleplay,
+        link: "https://play.google.com/store/apps/details?id=cl.figonzal.aaid",
+      },
+    ],
+  },
+  {
+    title: "EvalúaTool - Calculadora v2 cL",
+    description:
+      "Aplicación móvil que facilita el cálculo de puntuaciones de evaluaciones pertenecientes a la batería de evaluaciones psicopedagógicas Evalúa - Chile Versión 2.0",
+    image: "images/Projects/evalua.png",
+    buttons: [
+      {
+        title: "Github",
+        icon: FaGithub,
+        link: "https://github.com/figonzal1/Evaluatool",
+      },
+      {
+        title: "App",
+        icon: SiGoogleplay,
+        link: "https://play.google.com/store/apps/details?id=cl.figonzal.evaluatool",
+      },
+    ],
+  },
+];
+
+export { projectSeed };
---
+++ src/utils/types.ts
@@ -0,0 +1,13 @@
+import type { ReactElement } from "react";
+import type { IconType } from "react-icons";
+
+export type ProjectProp = {
+  title: string;
+  description: string;
+  image: string;
+  buttons: {
+    title: string;
+    icon: IconType;
+    link: string;
+  }[];
+};
