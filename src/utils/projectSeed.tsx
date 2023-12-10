7753d6a806657d686091db46b91746f184909ab3
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-12-10 19:25:11 -0300

    implemented locale & english language
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- src/utils/projectSeed.tsx
+++ src/utils/projectSeed.tsx
@@ -1,7 +1,7 @@
 import { FaGithub } from "react-icons/fa6/index.js";
 import { SiGoogleplay } from "react-icons/si/index.js";
 import type { ProjectProp } from "./types";
-import lqch  from "@assets/images/projects/lqch.png";
+import lqch from "@assets/images/projects/lqch.png";
 import aaid from "@assets/images/projects/aaid.png";
 import evalua from "@assets/images/projects/evalua.png";
 
@@ -62,4 +62,61 @@ const projectSeed: ProjectProp[] = [
   },
 ];
 
-export { projectSeed };
+const projectSeedEn: ProjectProp[] = [
+  {
+    title: "LastQuakeChile - Quakes in Chile",
+    description:
+      "It is an app where you can find out about the latest earthquakes that have occurred in Chile and receive notifications quickly on your cell phone.",
+    image: lqch,
+    buttons: [
+      {
+        title: "Github",
+        icon: FaGithub,
+        link: "https://www.github.com/figonzal1/LastQuakeChile",
+      },
+      {
+        title: "App",
+        icon: SiGoogleplay,
+        link: "https://play.google.com/store/apps/details?id=cl.figonzal.lastquakechile",
+      },
+    ],
+  },
+  {
+    title: "AAID",
+    description:
+      "This application allows the user to find the AAID code of their Android device with the option to easily copy it and share it through other applications.",
+    image: aaid,
+    buttons: [
+      {
+        title: "Github",
+        icon: FaGithub,
+        link: "https://github.com/figonzal1/AAID",
+      },
+      {
+        title: "App",
+        icon: SiGoogleplay,
+        link: "https://play.google.com/store/apps/details?id=cl.figonzal.aaid",
+      },
+    ],
+  },
+  {
+    title: "EvalúaTool - Calculator v2 cL",
+    description:
+      "Mobile application that facilitates the calculation of evaluation scores belonging to the battery of psychopedagogical evaluations Evalúa - Chile Version 2.0",
+    image: evalua,
+    buttons: [
+      {
+        title: "Github",
+        icon: FaGithub,
+        link: "https://github.com/figonzal1/Evaluatool",
+      },
+      {
+        title: "App",
+        icon: SiGoogleplay,
+        link: "https://play.google.com/store/apps/details?id=cl.figonzal.evaluatool",
+      },
+    ],
+  },
+];
+
+export { projectSeed, projectSeedEn };
